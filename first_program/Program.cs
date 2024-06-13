a0e7066fe5798792df8f067e0388f5b8a27a0bc2
uche181999 <uche181999@gmail.com>
2024-06-13 07:59:17 +0100

    added my first program

---
+++ first_program/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace first_program
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool  canI = true;
+            Console.WriteLine("biggest integer {0}",int.MaxValue);
+            Console.WriteLine("smallest integer {0}",int.MinValue);
+            
+
+
+        }
+    }
+}
