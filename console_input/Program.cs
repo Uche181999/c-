0429cf88df2667a38fc4aa79c6b6657aed807a5f
uche181999 <uche181999@gmail.com>
2024-06-17 09:54:17 +0100

    input tutorial

---
+++ console_input/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace console_input
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("please enter your name: ");
+            var name = Console.ReadLine();
+            Console.WriteLine($"welcome {name}");
+            Console.ReadKey();
+        }
+    }
+}
